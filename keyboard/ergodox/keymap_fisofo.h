4ac12ab59bf9dfb3be65dac059f60374fa0b45fe
fisofo <chris.sanny@gmail.com>
2013-09-15 21:52:23 -0500

    Further layout adjustment

--- keyboard/ergodox/keymap_fisofo.h
+++ keyboard/ergodox/keymap_fisofo.h
@@ -1,24 +1,24 @@
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap 0: Default Layer
-		See SVG file
+		See SVG file, doc/keymap_fisofo.svg
      */
 
-    KEYMAP(  // layout: layer 0: qwerty
+    KEYMAP(  // BLACK layout: layer 0: qwerty
         // left hand
         ESC,    1,   2,   3,   4,   5,   GRV,
         TAB,    Q,   W,   E,   R,   T,   FN1,
         LGUI,   A,   S,   D,   F,   G,
-        FN11,   Z,   X,   C,   V,   B,   ENT,
-        LCTL,LALT, APP,LALT,LCTL,
+        LSFT,   Z,   X,   C,   V,   B,   F15,
+        LCTL,LALT,CAPS,LALT,LCTL,
 								     VOLU,VOLD,
 										  PSCR,
-                                 FN3, FN7, INS,
+								BSPC, FN4, INS,
         // right hand
              EQL, 6,   7,   8,   9,   0,   MINS,
 			 FN2, Y,   U,   I,   O,   P,   BSLS,
                   H,   J,   K,   L,   SCLN,QUOT,
-			BSPC, N,   M,   COMM,DOT, SLSH,FN12,
-                       SPC, FN4,  APP,RALT,RCTL,
+			 F16, N,   M,   COMM,DOT, SLSH,RSFT,
+                       SPC, FN3,  APP,FN11,FN12,
         FN13,FN14,
         FN15,
          DEL, FN9,FN10
@@ -28,9 +28,9 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KEYMAP(  // layout: layer 1: colemak
         // left hand
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,   F,   P,   G, FN5,
-        TRNS,TRNS,   A,   R,   S,   D,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,   Q,   W,   F,   P,   G, FN5,
+        TRNS,   A,   R,   S,   T,   D,
+        TRNS,   Z,   X,   C,   V,   B,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
                                            TRNS,
@@ -38,15 +38,15 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
              TRNS,   J,   L,   U,   Y,SCLN,TRNS,
-			      TRNS,   N,   E,   I,   O,TRNS,
-             TRNS,   K,TRNS,TRNS,TRNS,TRNS,TRNS,
+			         H,   N,   E,   I,   O,TRNS,
+             TRNS,   K,   M,TRNS,TRNS,TRNS,TRNS,
                        TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,
         TRNS,
         TRNS,TRNS,TRNS
     ),
 
-	KEYMAP(  // layout: layer 2: gaming
+	KEYMAP(  // PINK layout: layer 2: gaming
         // left hand
         TRNS,TRNS,TRNS,TRNS,TRNS,  F5,  F8,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
@@ -67,28 +67,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS,TRNS,TRNS
     ),
 
-    KEYMAP(  // layout: layer 3: F-keys instead of numbers
-        // left hand
-        FN16,  F1,  F2,  F3,  F4,  F5, F12,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
-        TRNS,TRNS,TRNS,TRNS,TRNS,
-                                      TRNS,TRNS,
-                                           TRNS,
-                                   NO,TRNS,TRNS,
-        // right hand
-		   FN19,  F6,  F7,  F8,  F9, F10, F11,
-             NO,  NO,HOME,  UP, END,  NO,TRNS,
-                  NO,LEFT,DOWN,RGHT,  NO,TRNS,
-           TRNS,  NO,PGUP,  NO,PGDN,  NO,TRNS,
-                     TRNS,  NO,TRNS,TRNS,TRNS,
-        FN17,FN18,
-        TRNS,
-        TRNS,TRNS,NO
-    ),
-
-    KEYMAP(  // layout: layer 4: mouse + numpad
+    KEYMAP(  // BLUE layout: layer 3: mouse + numpad
         // left hand
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,BTN1,MS_U,BTN2,WH_U,TRNS,
@@ -97,7 +76,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS,TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
                                            TRNS,
-                                 TRNS,  NO,TRNS,
+                                 TRNS,TRNS,TRNS,
         // right hand
              TRNS,  NO,NLCK,PSLS,PAST,PMNS,TRNS,
              TRNS, ESC,  P7,  P8,  P9,PPLS,TRNS,
@@ -106,7 +85,28 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          P0, FN8,PDOT,PENT,TRNS,
         TRNS,TRNS,
         TRNS,
-        TRNS,  NO,TRNS
+        TRNS,TRNS,TRNS
+    ),
+
+    KEYMAP(  // RED layout: layer 4: F-keys instead of numbers
+        // left hand
+        FN16,  F1,  F2,  F3,  F4,  F5, F12,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,
+                                      TRNS,TRNS,
+                                           TRNS,
+								 TRNS,TRNS,TRNS,
+        // right hand
+		   FN19,  F6,  F7,  F8,  F9, F10, F11,
+             NO,  NO,HOME,  UP, END,  NO,TRNS,
+                  NO,LEFT,DOWN,RGHT,  NO,TRNS,
+           TRNS,  NO,PGUP,  NO,PGDN,  NO,TRNS,
+                     TRNS,  NO,TRNS,TRNS,TRNS,
+        FN17,FN18,
+        TRNS,
+        TRNS,TRNS,TRNS
     ),
 
 /*
@@ -165,22 +165,22 @@ enum function_id {
  * Fn action definition
  */
 static const uint16_t PROGMEM fn_actions[] = {
-    ACTION_LAYER_SET(0, ON_BOTH),     		     	// FN0  - Set to  layer 0|Qwerty; unused
+    ACTION_LAYER_SET(0, ON_BOTH),     		     	// FN0  - UNUSED; Set to  layer 0|Qwerty
     ACTION_LAYER_ON(1, ON_PRESS),					// FN1  - turn on layer 1|Colemak
     ACTION_LAYER_ON(2, ON_PRESS),	       			// FN2  - turn on Layer 2|Gaming
-    ACTION_LAYER_TAP_KEY(3, KC_BSPC),				// FN3  - Tap = BackSpace / Hold = Layer 3|Navigation+F-keys
-	ACTION_LAYER_ON(4, ON_PRESS),   				// FN4  - turn on Layer 4|Numpad+Mouse
+	ACTION_LAYER_ON(3, ON_PRESS),   				// FN3  - turn on Layer 3|Numpad+Mouse
+	ACTION_LAYER_TAP_KEY(4, KC_DEL),				// FN4  - Tap = BackSpace / Hold = Layer 4|Navigation+F-key
 
 	ACTION_LAYER_OFF(1, ON_PRESS),					// FN5  - turn off layer 1|Colemak
 	ACTION_LAYER_OFF(2, ON_PRESS),		       		// FN6  - turn off Layer 2|Gaming
-	ACTION_LAYER_TAP_KEY(4, KC_DEL),				// FN7  - Tap = Delete / Hold = Layer 4|Numbpad+Mouse
-	ACTION_LAYER_OFF(4, ON_PRESS),      			// FN8  - turn off Layer 4|Numbpad+Mouse
+	ACTION_LAYER_TAP_KEY(3, KC_DEL),				// FN7  - UNUSED; Tap = Delete / Hold = Layer 3|Numbpad+Mouse
+	ACTION_LAYER_OFF(3, ON_PRESS),      			// FN8  - turn off Layer 3|Numbpad+Mouse
 
-	ACTION_LAYER_TAP_KEY(4, KC_INS),				// FN9  - Tap = Insert / Hold = Layer 4|Numbpad+Mouse
-	ACTION_LAYER_TAP_KEY(3, KC_ENT),				// FN10 - Tap = Enter / Hold = Layer 3|Navigation+F-keys
+	ACTION_LAYER_TAP_KEY(3, KC_INS),				// FN9  - Tap = Insert / Hold = Layer 3|Numbpad+Mouse
+	ACTION_LAYER_TAP_KEY(4, KC_ENT),				// FN10 - Tap = Enter / Hold = Layer 4|Navigation+F-keys
 
-	ACTION_MODS_TAP_KEY(MOD_LSFT, KC_LBRC),         // FN11 - Tap = [ { / Hold = LShift
-	ACTION_MODS_TAP_KEY(MOD_RSFT, KC_RBRC),         // FN12 - Tap = ] } / Hold = RShift
+	ACTION_MODS_TAP_KEY(MOD_RALT, KC_LBRC),         // FN11 - Tap = [ { / Hold = RAlt
+	ACTION_MODS_TAP_KEY(MOD_RCTL, KC_RBRC),         // FN12 - Tap = ] } / Hold = RCtrl
 
     ACTION_MODS_KEY(MOD_LGUI, KC_LEFT),				// FN13 - Win+Left
 	ACTION_MODS_KEY(MOD_LGUI, KC_RGHT),				// FN14 - Win+Right
